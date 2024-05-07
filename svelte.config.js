67cc5e5bbd7a34de506e5476881d1c7dd31f1a4c
chcknlegwill <willn70@outlook.com>
2024-05-07 17:47:35 +0100

    I'm done

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-cloudflare';
 
 import  sveltePreprocess from 'svelte-preprocess';
 //^ getting sass (css) imported
