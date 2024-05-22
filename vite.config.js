3d1398e2498de2e81b919796c61487a917d2d9bd
chcknlegwill <willn70@outlook.com>
2024-05-22 16:39:09 +0100

    Tried to get it working but not enough time, going to work on this after exams

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()]
-});
+const config = {
+	plugins: [sveltekit()],
+	ssr: {
+		noExternal: ['three', 'troika-three-text']
+	}
+};
+
+export default config;
