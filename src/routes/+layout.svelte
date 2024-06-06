5e2f0a528fef848b24523356947def2a22bcfcf2
chcknlegwill <willn70@outlook.com>
2024-06-06 23:03:58 +0100

    Should fix the font issue on phones & fixed import errors (using old af git repo for fonts).

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,16 @@
 <!--layout - applys globally-->
 
-<script>
+<script lang="ts">
     //import Header from '$lib/components/header.svelte';
     //import Body from '$lib/components/main.svelte'
     //these are global imports and effect every page - need
     //seperate components for blog and projects (& other pages after that)
+
 </script>
 
 
+<svelte:head>
+</svelte:head>
 <main>
     <slot />
 </main>
@@ -15,3 +18,9 @@
 the header and the footer (main content in the middle)-->
 
 <footer><!--footer content goes here--></footer>
+
+<style>
+    *{
+        font-family: 'Fira Code';
+    }
+</style>
