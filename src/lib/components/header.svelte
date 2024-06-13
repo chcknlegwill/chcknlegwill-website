a7d4fd7c91eefff3d03a854d8f21ec463ce0e668
chcknlegwill <willn70@outlook.com>
2024-06-13 21:56:54 +0100

    From laptop - back home

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -8,7 +8,7 @@
     <div class="title-container">
         <a href="/"><h1 class="title">Chcknlegwill</h1></a>
     </div>
-    <div class="github-placeholder"  id="github">
+    <div class="github">
         <!--^ this is just a placeholder for now,
         mainly to get the header to stay in the middle with
         the github image on the left screwing up the flexbox
