7dfbcd7fef1265bb9f7ece06f2c6b29c3175c90a
chcknlegwill <willn70@outlook.com>
2024-11-13 17:33:17 +0000

    Icon is now definitley in the tab section of the website.

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,10 @@
 import adapter from '@sveltejs/adapter-cloudflare';
-
 import  sveltePreprocess from 'svelte-preprocess';
 //^ getting sass (css) imported
 import { mdsvex } from "mdsvex"; //importing mdsvex (for markdown support)
+
+
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,7 +12,18 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		prerender: {
+			handleHttpError: ({ path, referrer, message }) => {
+				// ignore deliberate link to shiny 404 page
+				if (path === '/not-found' && referrer === '/blog/how-we-built-our-404-page') {
+					return;
+				}
+
+				// otherwise fail the build
+				throw new Error(message);
+			}
+		}
 	},
 	
 	extensions: [".svelte", ".md"],
