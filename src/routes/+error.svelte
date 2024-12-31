f54e76a1e3257238770827feff9dc972e8946dfa
chcknlegwill <willn70@outlook.com>
2024-12-31 01:45:38 +0000

    Added some parts to the main page and 404 page.

---
+++ src/routes/+error.svelte
@@ -0,0 +1,33 @@
+<script></script>
+
+<main class="main">
+    <div class="container">
+        <h1>404</h1>
+        <h1>Not Found</h1>
+    </div>
+    
+</main>
+
+<style>
+    .main {
+        margin-top: 1%;
+        display: flex;
+        flex-direction: column;
+    }
+
+    .container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+    h1 {
+        font-size: 39px;
+        color: #ebdbb1;
+    }
+
+    h0 {
+        font-size: 59px;
+        color: #cc240d;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  //import stuff here
+</script>
+
+<!-- Main for the main content (for flexbox)-->
+<main>
+  <div class="intro">
+    <p>Hello. Welcome to my site. You can find more about me on the 
+      <a href="/about" id="link">>About</a> page, read something on the 
+      <a href="/blog" id="link" >>blog </a> or -> explore some of my 
+      <a href="/projects" id="link" >>projects </a>(or use the buttons below).</p>
+    <p>To go back to the previous page, all you need to do is click the heading at the top (Chcknlegwill in this case) and you'll be sent to the previous page.
+    Or you can use the newly added navigation bar just below the header to go to different pages.</p>
+  </div>
+
+
+  <div class="buttons-container">
+    <div class="buttons">
+      <a href="/blog">
+        <button class="button">>blog</button>
+      </a>
+    </div>
+    <div class="buttons">
+      <a href="/about">
+        <button class="button">>about</button>
+      </a>
+    </div>
+    <div class="button">
+      <a href="/projects">
+      <button class="button">>projects</button>
+      </a>
+    </div>
+  </div>
+</main>
