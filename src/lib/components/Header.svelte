daa4eb46578d774d4b7b4ae57e14d20c48bcf626
chcknlegwill <willn70@outlook.com>
2025-01-02 20:53:12 +0000

    Added main header as well as some basic structural css, changed back over to npm as yarn was causing issues deploying on cloudflare - this should deploy fine

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,31 @@
+<script>
+
+</script>
+
+<header>
+  <div class="header">
+    <div class="links">
+      <a href="https://github.com/chcknlegwill/" target="_blank">
+        <!-- put github image here -->
+      </a>
+    </div>
+
+
+      <a href="/">
+        <h1 class="title">chcknlegwill</h1>
+      </a>
+
+    <div class="links">
+      <!--placeholder to make title look even-->
+    </div>
+  </div>
+</header>
+
+<!-- also  implement the nav bar later on -->
+
+
+
+
+<style>
+  @import "./styles/headers.css";
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,43 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import Header from "$lib/components/Header.svelte";
+</script>
+
+<svelte:head>
+  <title>William - personal website</title>
+</svelte:head>
+
+
+<Header />
+<main>
+  <div class="intro">
+    <p>Hello, Welcome to by site. You can find more about me on the <a href="/about" id="link">>About</a>
+    page, read something on the <a href="/blog" id="link" >>blog </a> or -> explore some of my <a href="/projects" id="link" >>projects </a>
+   (or use the buttons below).</p>
+    <p>To go back to the previous page, all you need to do is click the heading at the top (Chcknlegwill in this case) and you'll be sent to the previous page.
+    Or you can use the newly added navigation bar just below the header to go to different pages.
+  </p>
+  </div>
+
+  <div class="buttons-container">
+    <div class="button">
+      <a href="/blog">
+        <button>>blog</button>
+      </a>
+    </div>
+    <div class="button">
+      <a href="about">
+        <button>>about</button>
+      </a>
+    </div>
+    <div class="button">
+      <a href="projects">
+        <button>>projects</button>
+      </a>
+    </div>
+  </div>
+</main>
+
+
+<div class="rand-container">
+  <!-- implement the random blog or project article here later (need the base structure first) -->
+</div>
