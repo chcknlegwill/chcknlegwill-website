b7ddad87204dab59f3b84dccd843d39db1a88a98
chcknlegwill <WillN70@outlook.com>
2025-04-06 20:06:34 +0100

    Changing language to go to see if developing it in go is any better...

--- build/client/_app/immutable/nodes/3.8l7n3WRt.js
+++
@@ -1,1 +0,0 @@
-import{s}from"../chunks/scheduler.DtGJeGpk.js";import{S as t,i as e}from"../chunks/index.DEgs2rZ3.js";class l extends t{constructor(o){super(),e(this,o,null,null,s,{})}}export{l as component};
--- build/server/chunks/_layout.svelte-D0OG3u66.js
+++
@@ -1,8 +0,0 @@
-import { c as create_ssr_component } from './ssr-BWVJttvk.js';
-
-const Layout = create_ssr_component(($$result, $$props, $$bindings, slots) => {
-  return `  <main>${slots.default ? slots.default({}) : ``}</main>  <footer data-svelte-h="svelte-ui690l"></footer>`;
-});
-
-export { Layout as default };
-//# sourceMappingURL=_layout.svelte-D0OG3u66.js.map
