cb3a5675e6d5eca08dcc1dc7469a376a2ba53c60
William <WillN70@outlook.com>
2025-04-08 01:07:47 +0100

    Going to have a LOT of editing to do...

--- svelte.config.js
+++ svelte.config.js
@@ -1,29 +1,11 @@
 import adapter from '@sveltejs/adapter-cloudflare';
-import  sveltePreprocess from 'svelte-preprocess';
-//^ getting sass (css) imported
-import { mdsvex } from "mdsvex"; //importing mdsvex (for markdown support)
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { mdsvex } from 'mdsvex';
 
-
-
-
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
-	},
-	
-	extensions: [".svelte", ".md"],
-
-	preprocess: [
-		sveltePreprocess(), //sass
-    mdsvex({
-			extensions: [".md"]
-		}),
-    
-	] 
+export default {
+  extensions: ['.svelte', '.md'],
+  preprocess: [vitePreprocess(), mdsvex({ extensions: ['.md'] })],
+  kit: {
+    adapter: adapter()
+  }
 };
-
-export default config;
